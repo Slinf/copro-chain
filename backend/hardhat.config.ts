5f4eeb2f424d13792f7cc2db0d2de69e4c31b95e
Clement-Bretin <165793772+Clement-Bretin@users.noreply.github.com>
2025-04-05 19:43:30 +0200

    🔧 backend config for sepolia

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -2,6 +2,14 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "solidity-docgen";
 
+require("hardhat-gas-reporter");
+require("dotenv").config();
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
+
+console.log(PRIVATE_KEY.length);
 const config: HardhatUserConfig = {
   solidity: {
     version: "0.8.28",
@@ -12,11 +20,27 @@ const config: HardhatUserConfig = {
       },
     }
   },
+  defaultNetwork: "hardhat",
   networks: {
     hardhat: {
       chainId: 31337,
     },
-  }
+    localhost: {
+      url: "<http://127.0.0.1:8545>",
+      chainId: 31337,
+    },
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 11155111,
+    }
+  },
+  gasReporter: {
+    enabled: true,
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
 };
 
 export default config;
